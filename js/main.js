6254976a5cae8792b4fd7bf34cd7663b7796e9f5
Lennart Syré <lenno@Lias-Air.fritz.box>
2020-05-23 17:05:55 +0200

    random onload color (commented out)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 
+    // $(window).on('load', function(){
+    //     let colors = ['#b3b3ff', '#ffb3ff', '#b3e9ff'];
+    //     // var body = $(body);
+
+    //     // for(var i=0; i<colors.length; i++){
+    //     //     Math.floor(Math.random()*colors.length);
+    //     // }
+
+    //     let colorOne = colors[0];
+
+    // });
+
+
     var contact  = $('#assist-contact');
     var info  = $('#assist-info');
 
     $('#assist').on('click', function(){
-
         if (contact.hasClass('active')) {
             contact.toggleClass('active', false);
             info.toggleClass('active', false);
@@ -12,12 +24,10 @@ $(document).ready(function(){
             contact.toggleClass('active', true);
             info.toggleClass('active', true);
         }
-
     });
 
     $(window).on('scroll', function(){
         contact.toggleClass('active', false);
         info.toggleClass('active', false);
     });
-
 });
