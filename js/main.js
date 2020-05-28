94f8aef238491bdfb497f49c4ae942aeecc84699
Ginsecurity <syrelennart@gmail.com>
2020-05-28 21:56:40 +0200

    scrollbar on chrome; image folder reconstruction;  minor fixes

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     //Assist
     var contact  = $('#assist-contact');
     var info  = $('#assist-info');
+    var contactInner = $('.contact');
+    var infoInner = $('.info');
 
     $('#assist').on('click', function(){
-        if (contact.hasClass('active')) {
+        if (contact.hasClass('active') || info.hasClass('active') || contactInner.hasClass('active') || infoInner.hasClass('active')) {
             contact.toggleClass('active', false);
             info.toggleClass('active', false);
+            contactInner.toggleClass('active', false);
+            infoInner.toggleClass('active', false);
         } else {
             contact.toggleClass('active', true);
             info.toggleClass('active', true);
@@ -15,22 +19,21 @@ $(document).ready(function(){
     });
 
     $(contact).on('click', function(){
-        $('.contact').toggleClass('active', true);
-        $('.info').toggleClass('active', false);
+        contactInner.toggleClass('active', true);
+        infoInner.toggleClass('active', false);
     });
 
     $(info).on('click', function(){
-        $('.info').toggleClass('active', true);
-        $('.contact').toggleClass('active', false);
+        infoInner.toggleClass('active', true);
+        contactInner.toggleClass('active', false);
     });
-
 
     $(window).on('scroll', function(){
         setTimeout(function(){
             contact.toggleClass('active', false);
             info.toggleClass('active', false);
         }, 300);
-        $('.contact').toggleClass('active', false);
-        $('.info').toggleClass('active', false);
+        contactInner.toggleClass('active', false);
+        infoInner.toggleClass('active', false);
     });
 });
